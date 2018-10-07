b60a7ce28c3bfe32b16e4491c8d0b4719d25c180
juliashalneva <yuliashalneva@yandex.ru>
2018-10-07 20:44:02 +0500

    Второе задание

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 1, 1, 1, 2, 1, 1, 1, 1, 1 };
+            Random rand = new Random();
+            //for (int i=0; i<array.Length; i++) array[i] = rand.Next();
+            int count = 1;
+            int max = 0;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    count++;
+                    if(max < count) max = count;
+                    //Console.WriteLine(count);
+                }
+                else
+                {
+                    if (max < count) max = count;
+                    count = 1;
+                }
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
